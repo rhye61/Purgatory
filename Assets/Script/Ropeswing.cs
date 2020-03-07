0a7a21dad3769665ef8974476cee31487ecbd039
rhye61 <56714989+rhye61@users.noreply.github.com>
2020-03-07 12:34:19 +0800

    Rhye 07/03
    1-1

---
+++ Assets/Script/Ropeswing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ropeswing : MonoBehaviour
+{
+    public bool isGrabbed = false;
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
